5dc03fc55bbdd414a0df489deff898f1f1848175
Jan Horacek <jan.horacek@kmz-brno.cz>
2019-07-23 13:33:12 +0200

    Move docs to README.

--- README.md
+++ README.md
@@ -5,6 +5,16 @@ MTB library is used to set and get state of [MTB modules](http://mtb.kmz-brno.cz
 on model railroad via simple dll interface. It supports MTB-UNI and MTB-TTL
 modules with firmware 4.1.
 
+## Description
+
+ * This library works as a middleman between MTB-USB modules and computer
+   software. Is simplifies communication with the MTB.
+ * Implemented in Object Pascal, last compilation and editation in Delphi 2009 32-bit.
+ * Is usually exported as .dll file.
+ * MTB documentation is availabe somewhere nearby this library. If not,
+   send an e-mail to authors and they will send you the documentation.
+ * This library fully supports just MTB-UNI & MTB-TTL modules.
+
 See [project wiki](https://github.com/kmzbrnoI/mtb-lib/wiki) for more information.
 
 ## Authors
--- src/MTB.dpr
+++ src/MTB.dpr
@@ -22,35 +22,14 @@
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
-  limitations under the License.                                }
+   limitations under the License.
+}
 
-{ DESCRIPTON:
-    - This library works as a middleman between MTB-USB module and
-       computer software. Is simplifies communication with the MTB.
-    - Implemented in Object Pascal, last compilation and editation in Delphi 2009 32-bit
-    - Is usually exported as .dll file.
-    - MTB documentation is availabe somewhere nearby this library. If not,
-      send an e-mail to authors and they will send you the documentation.
-    - Most of the comments are written in czech (or better in czenglish),
-       so, be letient please.
-    - This file actually defines functions exported to parent application,
-       all technology is done in MTBusb.pas
-    - Errors are not reported via exceptions, but just via result values.
-    - !! This library supports JUST MTB-UNI, MTB-TTL and MTB-REG modules.
+{
+  DESCRIPTON:
 
- Small how-to:
-  1) connect MTB-USB board via USB
-  2) install modified FTDI drivers (see MTBD2XXUnit.pas)
-  3) load the library
-  4) select MTB-USB board
-  5) open device (by calling Open() from parent software)
-  6) configure modules (configuration will be saved)
-  7) start communication (by calling Start() from parent software)
-  8) use the bus  (SetInput, GetOutput, ...)
-  ...
-  9) stop communication by calling Stop()
-  10)close device by calling Close()
-
+  This file defines functions exported to parent application, all logic is done
+  in MTBusb.pas. Errors are not reported via exceptions, but just via result values.
 }
 
 
